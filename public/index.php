b213213bfa3e60bfcb5bc80a6ac5197d5cc180b0
Mikael Gramont <mgramont@gmail.com>
2017-03-19 12:08:30 +0100

    Fixing url-related bug.

--- public/index.php
+++ public/index.php
@@ -7,8 +7,7 @@ require_once('head.php');
 require_once('header.php');
 require_once('utils.php');
 define('BASE_URL', Utils::getBaseUrl());
-
-$currentPageId = Utils::getCurrentPageId($_SERVER['SCRIPT_URL']);
+$currentPageId = Utils::getCurrentPageId($_SERVER['REQUEST_URI']);
 $pageInfo = Utils::getPageInfo($MENU_ENTRIES, $currentPageId);
 ?>
 <html>
