ca0e17441b0fbfec857d1871f57db9bd40c66578
Mikael Gramont <mgramont@gmail.com>
2017-07-07 00:21:00 +0200

    Actually submitting the footer.

--- php/footer.php
+++ php/footer.php
@@ -2,17 +2,38 @@
 class Footer
 {
 	public static function container() {
+		$facsi = self::_sponsorImage("facsi-groupe.png");
+		$bovis = self::_sponsorImage("groupe-bovis.png");
+
+		$sponsors = [
+			'amc.png', 'arc.png', 'colab.png', 'hauts-de-france.png', 'hertz.png',
+			'mbs.png', 'trampa.png', 'venette.png', 'weotv-picardie.png',
+		];
+		$sponsorsHTML = "";
+		foreach ($sponsors as $sponsor) {
+			$img = self::_sponsorImage($sponsor);
+			$sponsorsHTML .= "<img src=\"${img}\" class=\"sponsor\" >\n";
+		}
+
 		return <<<HTML
 		<footer>
 			<p>Our partners</p>
-			<p>
-			<a href="https://www.facebook.com/AssociationMountainboardCompiegne/" target="_blank">AMC</a>
-			<a href="https://www.mountainboardworld.org/" target="_blank">IMA</a>
-			<a href="http://www.trampaboards.com/" target="_blank">Trampa</a>
+			<p class="sponsor-list">
+				<a href="http://www.facsi-groupe.fr/" target="_blank">
+					<img src="${facsi}" class="sponsor" alt="Facsi Groupe" >
+				</a>
+				<a href="http://www.groupe-bovis.com/fr/agence/porteurs-2000-95/" target="_blank">
+					<img src="${bovis}" class="sponsor" alt="Groupe Bovis" >
+				</a>
 			</p>
+			<p class="sponsor-list">$sponsorsHTML</p>
 		</footer>
 
 HTML;
 
-	}	
+	}
+
+	private static function _sponsorImage($name) {
+		return BASE_URL . "images/sponsors/" . $name;
+	}
 }
